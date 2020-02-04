8761e25b1387a2c7c81ae23d90503baccbf4f7fa
BradyRentingBIT <b.renting@student.bit-academy.nl>
2020-02-04 15:14:32 +0100

    Uitwerking van de exercise

---
+++ MyLogger.class.php
@@ -0,0 +1,46 @@
+<?php
+
+class MyLogger {
+
+    private $origin;
+
+    public function log($message, $type)
+    {
+        echo $this->logWithTime() . $type . ": " . $message . PHP_EOL;
+    }
+
+    public function warning($message)
+    {
+        echo $this->logWithTime() . $this->origin . " - WARNING: " . $message . PHP_EOL;
+    }
+
+    public function info($message)
+    {
+        echo $this->logWithTime() . $this->origin . " - INFO: " . $message . PHP_EOL;
+    }
+
+    public function error($message)
+    {
+        echo $this->logWithTime() . $this->origin . " - ERROR: " . $message . PHP_EOL;
+    }
+
+    public function debug($message)
+    {
+        echo $this->logWithTime() . $this->origin . " - DEBUG: " . $message . PHP_EOL;
+    }
+
+    private function logWithTime()
+    {
+        return "[" . date('m/d/Y h:i:s', time()) . "] ";
+    }
+
+    public function setOrigin($origin) {
+        $this->origin = $origin;
+    }
+
+}
+
+$logger = new MyLogger();
+
+$logger->setOrigin('TestClass');
+$logger->error('dit is een error');
